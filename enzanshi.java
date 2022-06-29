8ca012910fbdebf281166e932617fcd2be714275
souhei1118 <so.takashi1118@gmail.com>
2022-06-29 14:14:51 +0000

    [finish]java1-2

--- enzanshi.java
+++ enzanshi.java
@@ -3,5 +3,9 @@ public class enzanshi {
 	public static void main(String[] args) {
 		int number = (100 + 2) * 3;
 		System.out.println(number + number);
+
+		// 1234と5678を足し算して、それを3倍した結果を計算して出力
+		int number2 = (1234 + 5678) * 3;
+		System.out.println(number2);
 	}
 }
