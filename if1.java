85df862c07921fd505c8e7899810a1951aec4f2c
souhei1118 <so.takashi1118@gmail.com>
2022-06-30 12:32:13 +0000

    [add]java1-3

---
+++ if1.java
@@ -0,0 +1,8 @@
+// if文による条件分岐
+public class if1 {
+	public static void main(String[] args) {
+		int number =((int)(Math.random() * 3 + 1)) * 100;
+		System.out.println("あなたの得点は" + number + "ポイントです");
+		// ここにif文を追加する
+	}
+}
