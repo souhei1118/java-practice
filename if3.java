cb6277c92e4e6ab8ccd94d795d9b124d93f88cff
souhei1118 <so.takashi1118@gmail.com>
2022-06-30 12:55:28 +0000

    [add]java1-5

--- if3.java
+++ if3.java
@@ -1,10 +1,10 @@
 // if文による条件分岐　比較演算子
-public class Main {
+public class if3 {
 	public static void main(String[] args) {
 		int place = (int)(Math.random() * 12+ 1);
 		System.out.print(place + "位");
 		// ここにif文を追加する
-		if (place > 7) {
+		if (place < 7) {
 			System.out.println("入賞");
 		} else {
 			System.out.println("入賞圏外");
