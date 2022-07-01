f9d92c624a2df083a8ea1602495c4b6e3e83fb41
souhei1118 <so.takashi1118@gmail.com>
2022-07-01 14:18:59 +0000

    [add]java1-6

---
+++ omikuji.java
@@ -0,0 +1,11 @@
+// おみくじを作る
+public class Main {
+	public static void main(String[] args) {
+		int omikuji = (int)(Math.random() * 100 + 1);  // randomメソッドについては次のチャプターで説明します
+		if (条件式) {
+			System.out.println("omikujiの中身は" + omikuji + "なので大吉");
+		} else {
+			System.out.println("omikujiの中身は" + omikuji + "なので大凶");
+		}
+	}
+}
