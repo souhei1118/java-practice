370dcbaa0cf2041acc0d5018af2006dced4bdf93
souhei1118 <so.takashi1118@gmail.com>
2022-07-02 13:55:33 +0000

    [add]1-8

--- seireki.java
+++ seireki.java
@@ -1,5 +1,5 @@
 // 西暦年から昭和年を求める
-public class Main {
+public class seireki {
 	public static void main(String[] args) {
 		int seireki = (int)(Math.random() * 63 + 1926);
 		System.out.print("西暦" + seireki + "年は");
