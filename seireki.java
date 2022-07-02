8ed967ddbfdfd91e3f45b4d0a2fda3eea13b36ea
souhei1118 <so.takashi1118@gmail.com>
2022-07-02 13:57:42 +0000

    [finish]java1-8

--- seireki.java
+++ seireki.java
@@ -5,7 +5,7 @@ public class seireki {
 		System.out.print("西暦" + seireki + "年は");
 
 		// 昭和年を計算
-		int showa = 0;
+		int showa = seireki - 1925;
 		System.out.println("昭和" + showa + "年です。");
 	}
 }
