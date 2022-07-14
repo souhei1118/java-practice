09ca9043c912b924f6446cfc7234488908a58c42
souhei1118 <so.takashi1118@gmail.com>
2022-07-14 14:29:15 +0000

    [add]java1-11

---
+++ for1.java
@@ -0,0 +1,9 @@
+// forによるループ処理
+public class for1 {
+    public static void main(String[] args) {
+        int i;
+        for(i = 0; i <= 2; i++) {
+             System.out.println("hello world " + i);    // 繰り返し処理
+        }
+    }
+}
