e1b4017b246ce9d4bc9290528ae15ea8e80a98d1
souhei1118 <so.takashi1118@gmail.com>
2022-07-22 14:46:57 +0000

    [add]

--- while1.java
+++ while1.java
@@ -2,7 +2,7 @@
 public class while1 {
     public static void main(String[] args) {
         int i = 0;
-        while (i < 10) {
+        while (i < 20) {
             System.out.println("Hello World!!");
             i = i + 1;
         }
